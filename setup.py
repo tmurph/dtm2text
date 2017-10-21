cb1f7c72188a41020021015810079b7cfa534c8d
Trevor Murphy <trevor.m.murphy@gmail.com>
2017-10-21 16:55:15 -0700

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools as tools
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+tools.setup(
+    name='dtm2text',
+    version='0.0.1',
+    description='Dump Dolphin TAS Movie files to plain text, and construct DTM from the same.',
+    url='https://github.com/tmurph/dtm2text',
+    license='GPL3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities',
+    ],
+    long_description=readme(),
+    author='Trevor Murphy',
+    author_email='trevor.m.murphy@gmail.com',
+    packages=tools.find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+)

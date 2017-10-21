22f7d5367093bbc2eb649a92820017d5570e4309
Trevor Murphy <trevor.m.murphy@gmail.com>
2017-10-21 16:09:04 -0700

    Use iterables for text_from_bytes.
    We’re gonna need this to be a generator function, and it’s too much of a
    pain to write code that dynamically figures out if it’s getting just one
    value or a list of values.  It’s our code, so just assume we’re always
    passing a list / iterable, and then make sure we do, in fact, always
    pass a list / iterable.

--- dtm2text.py
+++ dtm2text.py
@@ -9,24 +9,27 @@ def byte_frames_from_file(movie):
 
 
 def text_frames_from_bytes(frame_data):
-    "Convert DTM frame data to text."
-    flags, l, r, a_x, a_y, c_x, c_y = struct.unpack("H6B", frame_data)
-    start_flag = flags & 1
-    a_flag = flags >> 1 & 1
-    b_flag = flags >> 2 & 1
-    x_flag = flags >> 3 & 1
-    y_flag = flags >> 4 & 1
-    z_flag = flags >> 5 & 1
-    d_up = flags >> 6 & 1
-    d_down = flags >> 7 & 1
-    d_left = flags >> 8 & 1
-    d_right = flags >> 9 & 1
-    l_flag = flags >> 10 & 1
-    r_flag = flags >> 11 & 1
-    button_data = (start_flag, a_flag, b_flag, x_flag, y_flag, z_flag,
-                   d_up, d_down, d_left, d_right, l_flag, r_flag, l, r,
-                   a_x, a_y, c_x, c_y)
-    return ":".join(str(b) for b in button_data)
+    """Convert sequential DTM frame data to text.
+
+    Assumes we always receive an iterable over frames."""
+    for frame in frame_data:
+        flags, l, r, a_x, a_y, c_x, c_y = struct.unpack("H6B", frame)
+        start_flag = flags & 1
+        a_flag = flags >> 1 & 1
+        b_flag = flags >> 2 & 1
+        x_flag = flags >> 3 & 1
+        y_flag = flags >> 4 & 1
+        z_flag = flags >> 5 & 1
+        d_up = flags >> 6 & 1
+        d_down = flags >> 7 & 1
+        d_left = flags >> 8 & 1
+        d_right = flags >> 9 & 1
+        l_flag = flags >> 10 & 1
+        r_flag = flags >> 11 & 1
+        button_data = (start_flag, a_flag, b_flag, x_flag, y_flag,
+                       z_flag, d_up, d_down, d_left, d_right, l_flag,
+                       r_flag, l, r, a_x, a_y, c_x, c_y)
+        yield ":".join(str(b) for b in button_data)
 
 
 def main(argv=None):

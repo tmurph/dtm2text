d62f637d2bd4a6552fca4e6cc4badf505f654eb9
Trevor Murphy <trevor.m.murphy@gmail.com>
2017-10-21 21:17:02 -0700

    Move iteration outside of frame processing.
    No need to think about iteration inside of the conversion function.

--- dtm2text.py
+++ dtm2text.py
@@ -14,28 +14,25 @@ def byte_frames_from_file(movie):
         yield frame
 
 
-def text_frames_from_bytes(frame_data):
-    """Convert sequential DTM frame data to text.
-
-    Assumes we always receive an iterable over frames."""
-    for frame in frame_data:
-        flags, l, r, a_x, a_y, c_x, c_y = struct.unpack("H6B", frame)
-        start_flag = flags & 1
-        a_flag = flags >> 1 & 1
-        b_flag = flags >> 2 & 1
-        x_flag = flags >> 3 & 1
-        y_flag = flags >> 4 & 1
-        z_flag = flags >> 5 & 1
-        d_up = flags >> 6 & 1
-        d_down = flags >> 7 & 1
-        d_left = flags >> 8 & 1
-        d_right = flags >> 9 & 1
-        l_flag = flags >> 10 & 1
-        r_flag = flags >> 11 & 1
-        button_data = (start_flag, a_flag, b_flag, x_flag, y_flag,
-                       z_flag, d_up, d_down, d_left, d_right, l_flag,
-                       r_flag, l, r, a_x, a_y, c_x, c_y)
-        yield ":".join(str(b) for b in button_data)
+def text_frame_from_bytes(frame_data):
+    "Convert DTM frame data to text."
+    flags, l, r, a_x, a_y, c_x, c_y = struct.unpack("H6B", frame_data)
+    start_flag = flags & 1
+    a_flag = flags >> 1 & 1
+    b_flag = flags >> 2 & 1
+    x_flag = flags >> 3 & 1
+    y_flag = flags >> 4 & 1
+    z_flag = flags >> 5 & 1
+    d_up = flags >> 6 & 1
+    d_down = flags >> 7 & 1
+    d_left = flags >> 8 & 1
+    d_right = flags >> 9 & 1
+    l_flag = flags >> 10 & 1
+    r_flag = flags >> 11 & 1
+    button_data = (start_flag, a_flag, b_flag, x_flag, y_flag,
+                   z_flag, d_up, d_down, d_left, d_right, l_flag,
+                   r_flag, l, r, a_x, a_y, c_x, c_y)
+    return ":".join(str(b) for b in button_data)
 
 
 def main(argv=None):
@@ -63,12 +60,8 @@ def main(argv=None):
             bin_header_file.write(header_bytes)
 
         with open(frames_path, mode='w') as text_frames_file:
-            # Parse the frame data.
-            byte_frames = byte_frames_from_file(bin_movie_file)
-            text_frames = text_frames_from_bytes(byte_frames)
-
-            # Write the results.
-            for text_frame in text_frames:
+            for byte_frame in byte_frames_from_file(bin_movie_file):
+                text_frame = text_frame_from_bytes(byte_frame)
                 text_frames_file.write(text_frame + "\n")
 
     return 0

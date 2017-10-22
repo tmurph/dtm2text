df34eabf463dd9607927b6a20ae7477795070d83
Trevor Murphy <trevor.m.murphy@gmail.com>
2017-10-21 21:47:49 -0700

    Fixup grammar and spacing in tests.

--- test_dtm2text.py
+++ test_dtm2text.py
@@ -2,6 +2,7 @@ import pytest
 
 import dtm2text
 
+
 @pytest.mark.parametrize("binary_data, text_data", [
     (b'\x00\x00\x00\x00\x80\x80\x80\x80',
      "0:0:0:0:0:0:0:0:0:0:0:0:0:0:128:128:128:128"),
@@ -14,7 +15,7 @@ import dtm2text
     (b'\x02\x00\x00\x00\xff\x80\x80\x80',
      "0:1:0:0:0:0:0:0:0:0:0:0:0:0:255:128:128:128"),
 ])
-def test_text_frames_from_bytes(binary_data, text_data):
+def test_text_frame_from_bytes(binary_data, text_data):
     "Can we parse frame data to text?"
-    text_frames = dtm2text.text_frame_from_bytes(binary_data)
-    assert text_data == text_frames
+    text_frame = dtm2text.text_frame_from_bytes(binary_data)
+    assert text_data == text_frame

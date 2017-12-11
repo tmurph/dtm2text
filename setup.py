9264d43c64bbe70d4637cec77f37a8b09a5017fa
Trevor Murphy <trevor.m.murphy@gmail.com>
2017-12-11 11:49:38 -0800

    Bump version number.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 tools.setup(
     name='dtm2text',
-    version='0.0.6',
+    version='0.0.7',
     description='Dump Dolphin TAS Movie files to plain text, and construct DTM from the same.',
     url='https://github.com/tmurph/dtm2text',
     license='GPL3',
